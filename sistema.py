5b0b61967c8252c20b608696903853d2714778e6
pedromoraes618 <pdmr123@hotmail.com>
2022-05-24 00:32:55 -0300

    sistema finalizado

--- sistema.py
+++ sistema.py
@@ -35,12 +35,12 @@ def cabecalhoMenu():
 # LIVRO
 def listarTodos():
     for livros in acervo:
-        cabecalho("Todos os livros em nosso acervo")
+        cabecalho("Todos os livros em nosso acervo || All books in our collection")
         print("Nome:", livros['nome'])
         print("codigo:", livros['codigo'])
         print("status:", livros['status'])
         cabecalhoMenu()
-    op = input("Digite (1) para voltar ")
+    op = input("Digite (1) para voltar || Type (1) to go back: ")
     if (op == "1"):
         menu()
     else:
@@ -48,7 +48,7 @@ def listarTodos():
 
 
 def cadastrarlivro():
-    codigo = input("codigo: ")
+    codigo = input("Código: ")
     nome = input("Nome: ")
     status = "Disponivel"
 
@@ -59,47 +59,59 @@ def cadastrarlivro():
 def consultarLivro(valor):
     for livros in acervo:
         if valor == livros['codigo']:
-            cabecalho("Resultado da busca")
+            cabecalho("Resultado da busca || Search result")
             print("Nome:", livros['nome'])
             print("codigo:", livros['codigo'])
             print("status:", livros['status'])
             cabecalhoMenu()
-        else:
-            print("Não temos esse livro em nosso acervo")
 
 
-def editarLivro(valor):
+
+def reservarLivro(valor):
     for livros in acervo:
         if valor == livros['codigo']:
-            op = input("Digite (1) para reservar o livro: ")
-            if (op == "1"):
-                livros['status'] = "Reservado"
-                print("Nome:", livros['nome'])
-                print("codigo:", livros['codigo'])
-                print("status:", livros['status'])
-                cabecalhoMenu()
-                print(bcolors.OK + "Alteração realizado com sucesso" + bcolors.RESET)
-            else:
-                print(bcolors.OK + "Operação cancelada" + bcolors.RESET)
-        else:
-            print(bcolors.WARNING + "Não temos esse livro em nosso acervo" + bcolors.RESET2)
+            # if(livros['status']=="Reservado"):
+            #     print(bcolors.WARNING +"O livro já está reservado"+ bcolors.RESET)
+            #     menu()
+            # else:
+                op = input(bcolors.WARNING + "Digite (1) para reservar o livro || Enter (1) to reserve the book: " + bcolors.RESET)
+                if (op == "1"):
+                    livros['status'] = "Reservado"
+                    print("Nome:", livros['nome'])
+                    print("codigo:", livros['codigo'])
+                    print("status:", livros['status'])
+                    cabecalhoMenu()
+                    print(bcolors.OK + "Livro reservado com sucesso || Book successfully booked" + bcolors.RESET)
+                else:
+                    print(bcolors.WARNING + "Operação cancelada || Operation canceled" + bcolors.RESET)
+        # else:
+        #     print(bcolors.WARNING + "Não temos esse livro em nosso acervo" + bcolors.RESET)
+        #     menu()
 
-    # LOGIN
+def excluirLivro(valor):
+    cont = -1
+    for livros in acervo:
+        cont = cont+1
+        if livros['codigo'] == valor:
+            del acervo[cont]
+            print(bcolors.OK + "Livro deletado com sucesso || Successfully deleted book" + bcolors.RESET)
+
+
 
 
 def cadastrarUsuario():
-    login = input("Digite o seu login: ")
-    senha = input("Digite a sua senha: ")
+    login = input("Digite o seu login || Enter your login: ")
+    senha = input("Digite a sua senha || Enter your password: ")
     dadosUsuario = {'login': login, 'senha': senha}
     usuario.append(dadosUsuario)
-    print(bcolors.OK + "Usuário cadastrado com sucesso! " + bcolors.RESET)
+    print(bcolors.OK + "Usuário cadastrado com sucesso || User registered successfully! " + bcolors.RESET)
 
 
 def validacaoLogin(usuarioLogin, usuarioSenha):
     for dadosUsuario in usuario:
         if usuarioLogin == dadosUsuario['login'] and usuarioSenha == dadosUsuario['senha']:
             limpar()
-            print(bcolors.OK + "Bem vindo: ", usuarioLogin + bcolors.RESET)
+            print(bcolors.OK + "Bem vindo || Welcome: ", usuarioLogin + bcolors.RESET)
             menu()
         else:
             print(bcolors.WARNING + "Usuário ainda não cadastrado" + bcolors.RESET)
@@ -108,45 +120,47 @@ def validacaoLogin(usuarioLogin, usuarioSenha):
 def menu():
     op = 0
     while op != "8":
-        op = input(bcolors.OK + ">>>Menu incial<<<" + bcolors.RESET + '''
+        op = input(bcolors.OK + ">>>Menu incial || Start Menu <<<" + bcolors.RESET + '''
 
-1- Cadastrar um Livro
-2- Consultar um Livro
-3- Listar todos os Livros
-4- alterar um livro
-5- Login
-6- Cadastrar
+1- Cadastrar um Livro || Register a Book
+2- Consultar um Livro || Consult a Book
+3- Listar todos os Livros || List all books
+4- Reservar um Livro || Resever a book
+5- Excluir Livro || Delete Book
+6- Cadastrar Usuário || Register user
 7- Logout
 
-Escolha uma opção: ''')
+Escolha uma opção || Choose an option: ''')
 
         if (op == "1"):
             cadastrarlivro()
             # Enquanto a variavel receber 1 será feito um loop onde Será perguntado ao usuario se ele irá cadastrar outro livro
-            opcaoCadastar = input("Desejar cadastrar outro livro (1) Sim (0) Voltar: ")
+            opcaoCadastar = input("Desejar cadastrar outro livro (1) Sim (0) Voltar || Want to register another book (1) Yes (0) Back:  ")
             while opcaoCadastar == "1":
                 cadastrarlivro()
-                opcaoCadastar = input("Desejar cadastrar outro livro (1) Sim (0) Voltar: ")
+                opcaoCadastar = input("Desejar cadastrar outro livro (1) Sim (0) Voltar || Want to register another book (1) Yes (0) Back: ")
 
         elif (op == "2"):
-            consultar = input('Digite o código do livro: ')
+            consultar = input('Digite o código do livro || Enter the book code:: ')
             if len(acervo) > 0:
                 consultarLivro(consultar)
                 # Enquanto a variavel receber 1 será feito um loop onde Será perguntado ao usuario se ele irá realozar a consulta novamente
-                opcaoConsultaUnitario = input("Dessaja realizar outra busca (1) Sim (0) Não: ")
+                opcaoConsultaUnitario = input("Dessaja realizar outra busca (1) Sim (0) Não || "
+                                              "Would you like to perform another search (1) Yes (0) No:  ")
                 while opcaoConsultaUnitario == "1":
                     if len(acervo) > 0:
-                        consultar = input('Digite o código do livro: ')
+                        consultar = input('Digite o código do livro || Enter the book code: ')
                         consultarLivro(consultar)
-                        opcaoConsultaUnitario = input("Dessaja realizar outra busca (1) Sim (0) Não: ")
+                        opcaoConsultaUnitario = input("Dessaja realizar outra busca (1) Sim (0) Não ||"
+                                                      " Would you like to perform another search (1) Yes (0) No: ")
                     else:
-                        print(bcolors.FAIL + "Nosso acervo está vazio" + bcolors.RESET)
+                        print(bcolors.FAIL + "Nosso acervo está vazio || Our collection is empty" + bcolors.RESET)
                         menu()
                 else:
                     menu()
 
             else:
-                print(bcolors.FAIL + "Nosso acervo está vazio" + bcolors.RESET)
+                print(bcolors.FAIL + "Nosso acervo está vazio || Our collection is empty" + bcolors.RESET)
                 menu()
 
 
@@ -155,20 +169,22 @@ Escolha uma opção: ''')
             if len(acervo) > 0:
                 listarTodos()
             else:
-                print(bcolors.FAIL + "Nosso acervo está vazio" + bcolors.RESET)
+                print(bcolors.FAIL + "Nosso acervo está vazio || Our collection is empty" + bcolors.RESET)
 
         elif (op == "4"):
             if len(acervo) > 0:
-                consultar = input('Digite o código do livro que deseja alterar: ')
-                editarLivro(consultar)
+                consultar = input('Digite o código do livro que deseja reservar || Enter the code of the book you want to book:: ')
+                consultarLivro(consultar)
+                reservarLivro(consultar)
 
             else:
-                print(bcolors.FAIL + "Nosso acervo está vazio" + bcolors.RESET)
+                print(bcolors.FAIL + "Nosso acervo está vazio || Our collection is empty" + bcolors.RESET)
 
 
         elif (op == "5"):
-            loginUsuario = input("Digite o seu login: ")
-            validacaoLogin(loginUsuario)
+            excluir = input("Digite o código do livro a ser excluido || Enter the book code to be unique: ")
+
+            excluirLivro(excluir)
 
         elif (op == "6"):
             cadastrarUsuario()
@@ -182,27 +198,30 @@ def login():
     # Tela login e cadastrado
     op = 0
     while op != "3":
-        op = input(bcolors.OK + ">>>Tela lOGIN<<<" + bcolors.RESET + '''
+        op = input(bcolors.OK + ">>>Tela lOGIN || Login Screen<<<" + bcolors.RESET + '''
 
         1- Login
-        2- Cadastrar
-        2- Sair do sistema
+        2- Cadastrar || register
+        3- Sair do sistema || Exit system
 
 
-        Escolha uma opção: ''')
+        Escolha uma opção || Choose an option:: ''')
 
         # LOGIN
         if (op == "1"):
-            loginUsuario = input("Digite o seu login: ")
-            senhaUsuario = input("Digite a sua Senha: ")
+            loginUsuario = input("Digite o seu login || Enter your login: ")
+            senhaUsuario = input("Digite a sua Senha || Enter your Password: ")
             if len(usuario) > 0:
                 validacaoLogin(loginUsuario, senhaUsuario)
 
             else:
-                cabecalho(bcolors.WARNING + "Sistema ainda não possui nenhum Usuário cadastrado" + bcolors.RESET)
+                cabecalho(bcolors.WARNING + "Sistema ainda não possui nenhum Usuário cadastrado || "
+                                            "System does not have any registered User yet." + bcolors.RESET)
         # CADASTRAR
         elif (op == "2"):
             cadastrarUsuario()
+    else:
+         print("Sair do sistema || Exit system")
 
 
 login()
